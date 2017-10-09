0871e7e4233d0af9fc714af6a99270a3a516bf50
bairen <577514861@qq.com>
2017-10-09 12:17:14 +0800

    22

---
+++ Application/Admin/Controller/PersonalController.class.php
@@ -0,0 +1,68 @@
+<?php
+namespace Admin\Controller;
+
+use Common\Controller\AdminBaseController;
+use Think\Controller;
+use Think\Exception;
+/*
+ * 个人设置（资料）
+ * */
+class PersonalController extends AdminBaseController
+{
+    /*
+     * 一个基本资料
+     * */
+    public function index()
+    {
+        $uid=session('user.id');
+        $data=M('Users')->field('id,username,email,phone')->where("id={$uid}")->find();
+
+        $res=M('AuthGroupAccess')->where("uid={$uid}")->find();
+        $group=M('AuthGroup')->where("id={$res['group_id']}")->find();
+
+        $this->assign('data',$data);
+        $this->assign('group',$group);
+        $this->display();
+    }
+
+    /*
+     * 修改个人资料
+     * */
+    public function edit()
+    {
+
+        try{
+            if(!IS_POST) throw new Exception('非法请求');
+            $username=I('post.username','','htmlspecialchars');
+            $phone=I('post.phone','','htmlspecialchars');
+            $email=I('post.email','','htmlspecialchars');
+            $password=I('post.password','','htmlspecialchars');
+            $password2=I('post.password2','','htmlspecialchars');
+            if(empty($username)) throw new Exception('登录名不能为空');
+
+            $map['username']=$username;
+            $map['phone']=$phone;
+            $map['email']=$email;
+            if(!empty($password) || !empty($password2)){
+                if($password != $password2) throw new Exception('两个密码不一致');
+                $map['password']=md5($password);
+            }
+            $uid=session('user.id');
+            $res=M('Users')->where("id={$uid}")->save($map);
+
+            if($res){
+                $this->success('修改成功',U('Personal/index'));
+            }else{
+                $this->error('修改失败');
+            }
+
+        }catch(Exception $e){
+            $message=$e->getMessage();
+            $this->error($message);
+        }
+
+
+    }
+
+
+}
