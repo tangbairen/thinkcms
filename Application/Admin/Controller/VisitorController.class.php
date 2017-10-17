771939c43a795ea9d5d5a10f4f9d7aaf363ab8bc
bairen <577514861@qq.com>
2017-10-17 11:09:31 +0800

    1221

--- Application/Admin/Controller/VisitorController.class.php
+++ Application/Admin/Controller/VisitorController.class.php
@@ -30,6 +30,6 @@ class VisitorController extends Controller
 
         //返回数据给接口方
         $data = array('cmd' => 'OK', 'token' => 'TOKEN');
-        echo json_encode($data);
+        return  json_encode($data);
     }
 }
