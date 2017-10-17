ba6a56871bb69fb7d179d8096629146e54cd8056
bairen <577514861@qq.com>
2017-10-17 14:59:55 +0800

    112211

--- visitor/Visitor.class.php
+++ visitor/Visitor.class.php
@@ -8,8 +8,8 @@ class Visitor
     private $conf=array(
         'host'=>'localhost',
         'port'=>'3306',
-        'user'=>'root',
-        'passwd'=>'root',
+        'user'=>'thinkcms',
+        'passwd'=>'bairen168',
         'dbname'=>'thinkcms'
     );
 
