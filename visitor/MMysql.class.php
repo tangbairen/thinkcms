475f2ed0a36ac3203b83fab23afd99fbb8315d36
bairen <577514861@qq.com>
2017-10-17 16:24:34 +0800

    1112

--- visitor/MMysql.class.php
+++ visitor/MMysql.class.php
@@ -190,7 +190,6 @@ class MMysql {
         $data = $this->_dataFormat($tbName,$data);
         if (!$data) return;
         $sql = "insert into ".$tbName."(".implode(',',array_keys($data)).") values(".implode(',',array_values($data)).")";
-        echo $sql;exit;
         return $this->_doExec($sql);
     }
 
