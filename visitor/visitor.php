8d26a1b50368406237f4f447fcbd6311b96401b3
bairen <577514861@qq.com>
2017-10-17 12:32:43 +0800

    112

--- visitor/visitor.php
+++ visitor/visitor.php
@@ -18,5 +18,9 @@
 
     }
 
+    $time=date('Y-m-d H:i:s',time());
+    file_put_contents("./Uploads/log/".$time.'.txt', $content);
+
+
     $data= array('cmd'=>'OK','token'=>'TOKEN');
     echo json_encode($data);
