00c450754409e104eff103849ddae6f91ba766b6
bairen <577514861@qq.com>
2017-10-17 15:50:52 +0800

    1112

--- visitor/visitor.php
+++ visitor/visitor.php
@@ -2,15 +2,13 @@
     header('Content-type:text/html;charset=utf-8');
     error_reporting(1);
     require('./Visitor.class.php');
-    $mysql=new MMysql(array(
+    /*$mysql=new MMysql(array(
         'host'=>'localhost',
         'port'=>'3306',
         'user'=>'thinkcms',
         'passwd'=>'bairen168',
         'dbname'=>'thinkcms'
-    ));
-
-    var_dump($mysql);
+    ));*/
 
     $content='%7B%22guest_name%22%3A%22%E4%BB%98%E5%AE%B6%E5%A6%AE8771%2BT%2318720220716%22%2C%22email%22%3A%22%22%2C%22province%22%3A%22%22%2C%22city%22%3A%22%22%2C%22qq%22%3A%22%22%2C%22mobile%22%3A%22%22%2C%22address%22%3A%22%22%2C%22remark%22%3A%22%22%2C%22guest_id%22%3A%2210575296314018%22%2C%22zipcode%22%3A%22%22%2C%22worker_id%22%3A%22bote53kf08%40163.com%22%2C%22tag%22%3A%22%22%2C%22time%22%3A1508147949%2C%22company_id%22%3A%2272133840%22%2C%22cmd%22%3A%22customer%22%2C%22token%22%3A%22TOKEN%22%7D1794267d-98fb-41d5-8407-1ae2de465cb4';
     $strData=@urldecode($content);
@@ -22,11 +20,9 @@
     $visitor=new Visitor();
 
     if(count($data) == count($data,1)){//一维（访客信息）
-        echo 111;
         $visitor->addInfo($data);
 
     }else{//二维（访客聊天记录）
-        echo 122;
         $visitor->addRecord($data);
     }
     $time=date('Y-m-d H:i:s',time());
