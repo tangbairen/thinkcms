466159a0afee672050025f2899fe5afbed05643d
bairen <577514861@qq.com>
2017-10-17 10:07:56 +0800

    1311

--- Application/Admin/Model/AreaModel.class.php
+++ Application/Admin/Model/AreaModel.class.php
@@ -12,7 +12,7 @@ class AreaModel extends Model
             -> alias('a')
             ->field('a.id,a.area_name,GROUP_CONCAT(p.`name`) as province')
             ->join('left join bt_province as p on a.id=p.area_id')
-            ->group('a.area_name')
+            ->group('a.id,a.area_name')
             ->order('a.id')
             ->where('a.id > 0')
             ->select();
