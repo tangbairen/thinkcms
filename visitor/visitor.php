9b62f4a3bfe7bc740b3bc4cb133b43a6d43cd0be
bairen <577514861@qq.com>
2017-10-21 09:29:37 +0800

    1122

--- visitor/visitor.php
+++ visitor/visitor.php
@@ -1,10 +1,27 @@
 <?php
-    header('Content-type:text/html;charset=utf-8');
+    /*header('Content-type:text/html;charset=utf-8');
     error_reporting(0);
-    set_time_limit(0);
-    require('./Visitor.class.php');
+    set_time_limit(0);*/
+    header('content-type:text/html; charset=utf-8');
+    //开启所有的错误报告
+    error_reporting(-1);
+    set_time_limit(180);
+    //设置时区
+    ini_set('date.timezone', 'PRC');
+    //禁用页面显示错误
+    ini_set('display_errors', 0);
+    //开启日志记录功能
+    ini_set('log_errors', 1);
+    //设置错误日志保存的位置
+    ini_set('error_log', '../Uploads/log/error.log');
+    //忽略重复的错误
+//    ini_set('ignore_repeated_errors', 'on');
+    //忽略重复的错误来源
+//    ini_set('ignore_repeated_source', 'on');
+
 
     if($_POST){
+        require('./Visitor.class.php');
         sleep(0.5);
         $visitor=new Visitor();
         $cmd=isset($_POST['cmd']) ? $_POST['cmd'] : '';
