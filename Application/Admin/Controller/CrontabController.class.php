46b8338b5f29b63d52de7e490192eaf3cec96c5e
bairen <577514861@qq.com>
2017-10-22 08:29:49 +0800

    1122

--- Application/Admin/Controller/CrontabController.class.php
+++ Application/Admin/Controller/CrontabController.class.php
@@ -16,7 +16,7 @@ class CrontabController extends Controller
     public function allot()
     {
         $time=time();//当前时间
-        $beterTime=$time - 7200000;//前一小时的时间
+        $beterTime=$time - (3600*24);//前一天的时间
 
         $map['status']=1;
         $map['time']=array('between',array($beterTime,$time));
