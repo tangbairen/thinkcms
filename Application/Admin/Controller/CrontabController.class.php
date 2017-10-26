84812726629f30db9b6bd0a8bb6c146ca0cc15ab
bairen <577514861@qq.com>
2017-10-26 19:30:59 +0800

    12121

--- Application/Admin/Controller/CrontabController.class.php
+++ Application/Admin/Controller/CrontabController.class.php
@@ -16,10 +16,10 @@ class CrontabController extends Controller
     public function allot()
     {
         $ip = get_client_ip();
-        /*if($ip != '47.52.59.51'){
+        if($ip != '47.52.59.51'){
 
             return false;
-        }*/
+        }
 
         $time=time();//当前时间
         $beterTime=$time - (3600*24);//前一天的时间
