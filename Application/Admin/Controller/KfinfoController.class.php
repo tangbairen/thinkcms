db70cafdf0a2e1a55173c8e9bcff6f1ee6840644
bairen <577514861@qq.com>
2017-11-06 17:29:19 +0800

    23232

--- Application/Admin/Controller/KfinfoController.class.php
+++ Application/Admin/Controller/KfinfoController.class.php
@@ -18,7 +18,7 @@ class KfinfoController extends Controller
             if(!IS_POST) throw new Exception('非法请求');
             $aaa=$_POST;
             $time=date('Y-m-d H:i:s',time()).'_'.rand();
-            file_put_contents("./Uploads/log/bote_".$time.'.txt', $aaa);
+            //file_put_contents("./Uploads/log/bote_".$time.'.txt', $aaa);
 
             $ip = get_client_ip();
             //if($ip != '122.227.58.170')throw new Exception('非法请求');
@@ -50,7 +50,7 @@ class KfinfoController extends Controller
             echo json_encode($data);
         }catch(Exception $e){
             $message=$e->getMessage();
-            file_put_contents('./Uploads/log/message.txt',$message);
+            //file_put_contents('./Uploads/log/message.txt',$message);
             $data= array('cmd'=>'OK','token'=>'TOKEN');
             echo json_encode($data);
         }
@@ -72,7 +72,7 @@ class KfinfoController extends Controller
             //if($ip != '122.227.58.170')throw new Exception('非法请求');
 
             $cmd=I('post.cmd','');
-            file_put_contents("./Uploads/log/ditiecmd_".$time.'.txt', $cmd);
+            //file_put_contents("./Uploads/log/ditiecmd_".$time.'.txt', $cmd);
             if($cmd == 'talk_info'){//整体推送
                 $content=I('post.content','');
                 if(!empty($content)){
@@ -84,7 +84,7 @@ class KfinfoController extends Controller
             }else{
                 //访客信息推送
                 $customer=I('post.content','');
-                file_put_contents("./Uploads/log/ditiectomer_".$time.'.txt', $customer);
+                //file_put_contents("./Uploads/log/ditiectomer_".$time.'.txt', $customer);
                 if(!empty($customer)){
                     $customer=urldecode($customer);
                     $customer=json_decode($customer,true);
@@ -100,7 +100,7 @@ class KfinfoController extends Controller
             echo json_encode($data);
         }catch(Exception $e){
             $message=$e->getMessage();
-            file_put_contents('./Uploads/log/ditie_message.txt',$message);
+            //file_put_contents('./Uploads/log/ditie_message.txt',$message);
             $data= array('cmd'=>'OK','token'=>'TOKEN');
             echo json_encode($data);
         }
