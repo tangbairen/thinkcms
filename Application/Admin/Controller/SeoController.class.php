ec29256418bec56b3ed3f42cc9f15166f1432dc7
bairen <asdlfld@qq.com>
2017-12-08 00:03:03 +0800

    222

---
+++ Application/Admin/Controller/SeoController.class.php
@@ -0,0 +1,83 @@
+<?php
+namespace Admin\Controller;
+
+use Common\Controller\AdminBaseController;
+use Think\Exception;
+
+/*
+ * 处理SEO
+ * */
+class SeoController extends AdminBaseController
+{
+    /*
+     * 列表
+     * */
+    public function index()
+    {
+        $data=D('Resource')->selectData("source='SEO优化'");
+        //部门
+        $group=M('RoleDepartment')->select();
+        $phone=I('get.phone');
+        $s_group=I('get.group');
+        $start_time=I('get.start_time','');
+        $end_time=I('get.end_time','');
+        $brand_id=I('get.brand','');
+        $referer_id=I('get.referer','');
+
+        $allocation=I('get.allocation','');
+
+        $brand=M('Brands')->select();//品牌
+        $referer=M('Referer')->field('distinct title')->select();//来源渠道
+
+        $array=array(
+            'phone'=>$phone,
+            's_group'=>$s_group,
+            'start_time'=>$start_time,
+            'end_time'=>$end_time,
+            'allocation'=>$allocation,
+            'brand'=>$brand,
+            'brand_id'=>$brand_id,
+            'referer_id'=>$referer_id,
+            'referer'=>$referer
+        );
+
+        $this->assign($array);
+        $this->assign('group',$group);
+        $this->assign($data);// 赋值数据集
+        $this->display();
+
+    }
+
+    /*
+     * 添加数据
+     * */
+    public function add_data()
+    {
+        if(IS_POST){
+
+            try{
+                $res=D('Resource')->addData();
+                if(empty($res)) throw new Exception('添加失败');
+
+                $this->success('添加成功',U('Admin/seo/index'));
+            }catch(Exception $e){
+                $message=$e->getMessage();
+                $this->error($message);
+            }
+
+
+        }else {
+            //省份
+            $province = M('Province')->select();
+            //品牌
+            $brandArr = M('Brands')->order('order_by desc')->limit(45)->select();
+            $array = array(
+                'brandArr' => $brandArr,
+                'province' => $province
+            );
+
+            $this->assign($array);
+            $this->display();
+        }
+    }
+}
