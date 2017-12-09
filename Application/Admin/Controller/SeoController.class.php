ed55480c4cefce6ae2f3d0b723e86cf19ca95644
bairen <1656260142@qq.com>
2017-12-09 10:07:58 +0800

    3232

--- Application/Admin/Controller/SeoController.class.php
+++ Application/Admin/Controller/SeoController.class.php
@@ -14,7 +14,13 @@ class SeoController extends AdminBaseController
      * */
     public function index()
     {
-        $data=D('Resource')->selectData("source='SEO优化'");
+        $phone=I('get.phone','');
+        if(empty($phone)){
+            $where="source='SEO优化'";
+        }else{
+            $where='phone='.$phone;
+        }
+        $data=D('Resource')->selectData($where);
         //部门
         $group=M('RoleDepartment')->select();
         $phone=I('get.phone');
