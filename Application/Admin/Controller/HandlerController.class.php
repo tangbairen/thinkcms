232e4685b12bc1d221d284f6751a472e8077aa0f
bairen <1656260142@qq.com>
2017-12-18 19:52:56 +0800

    3232

--- Application/Admin/Controller/HandlerController.class.php
+++ Application/Admin/Controller/HandlerController.class.php
@@ -2,6 +2,7 @@
 namespace Admin\Controller;
 
 use Think\Controller;
+use Think\Exception;
 
 /*
  * 处理类
@@ -17,25 +18,82 @@ class HandlerController extends Controller
         $upload = new \Think\Upload();// 实例化上传类
         $upload->maxSize   =     3145728 ;// 设置附件上传大小
         $upload->exts      =     array('xls','xlsx');// 设置附件上传类型
-        $upload->savePath  =      './Uploads/excel/'; // 设置附件上传目录
+        $upload->savePath  =      './excel/'; // 设置附件上传目录
         // 上传单个文件
         $info   =   $upload->uploadOne($_FILES['myfile']);
-        if(!$info) {
-            // 上传错误提示错误信息
-            $this->error($upload->getError());
-        }else{// 上传成功 获取上传文件信息
-            $path=$info['savepath'].$info['savename'];
-            $this->readExcel($path);
 
+        try{
+
+            if(!$info) {
+                // 上传错误提示错误信息
+                //$this->error($upload->getError());
+                $message=$upload->getError();
+                throw new Exception($message);
+
+            }else{// 上传成功 获取上传文件信息
+                $path='./Uploads/'.$info['savepath'].$info['savename'];
+
+                $number_id=$this->readExcel($path);
+            }
+            $array=M('Import')->where('number_id='.$number_id)->select();
+            $repeatArray=M('Import')->query("select phone,count(*) as count from bt_resource group by phone having count>1");
+            if(!empty($array)){
+                foreach($array as $key=>&$val){
+                    if($val['group_id'] > 0){
+                        $res=M('RoleDepartment')->where("id={$val['group_id']}")->find();
+                        $val['group_name']=$res['name'];
+                        $val['fenpei']='已匹配';
+                    }else{
+                        $val['group_name']='';
+                        $val['fenpei']='未匹配';
+                    }
+                    if($val['brand_id'] > 0){
+                        $brands=M('Brands')->where("id={$val['brand_id']}")->find();
+                        $val['brands_name']=$brands['name'];
+                    }else{
+                        $val['brands_name']='';
+                    }
+                    if($val['province'] > 0){
+
+                        $province=M('Province')->where("id={$val['province']}")->find();
+
+                        $val['province_name']=$province['name'];
+                    }else{
+                        $val['province_name']='';
+                    }
+
+                    if(!empty($repeatArray)){
+                        foreach($repeatArray as $k=>$v){
+                            if($val['phone'] == $v['phone']){
+                                $val['repeart']='重复';
+                            }else{
+                                $val['repeart']='';
+                            }
+                        }
+                    }
+                }
+            }
+
+            $data=array('status'=>'success','data'=>$array,'number_id'=>$number_id);
+            $this->ajaxReturn($data);
+
+        }catch(Exception $e){
+
+            $message=$e->getMessage();
+            $data=array('status'=>'error','message'=>$message);
+
+            $this->ajaxReturn($data);
         }
+
     }
 
     //创建一个读取excel数据，可用于入库
     public function readExcel($path='002.xlsx')
     {
-        if(!file_exists($path)){
+
+        /*if(!file_exists(__ROOT__.$path)){
             return false;
-        }
+        }*/
 
         //引用PHPexcel 类
         import("Org.Util.PHPExcel");
@@ -65,8 +123,136 @@ class HandlerController extends Controller
         $allRow = $result->getHighestRow(); //行数
         $dataArray=$result->toArray();
 
+        $this->existTitle($dataArray[0]);
 
-        p($dataArray);
-//        return $dataArray;
+        $number_id=date("YmdHis"). mt_rand(1,100);//15-16 位
+
+        for($i=1;$i < count($dataArray);$i++){
+            if(!empty($dataArray[$i])){
+                $this->handlerData($dataArray[$i],$number_id);
+            }
+        }
+
+        return $number_id;
     }
+
+    /*
+     * 处理数据入库
+     * $data 一维数组
+     * $number_id   这次入库的编号
+     * */
+    public function handlerData($data,$number_id)
+    {
+        $kefu_id    =isset($data[0]) ? $data[0]:'';
+        $username   =isset($data[1]) ? $data[1]:'';
+        $address    =isset($data[2]) ? $data[2]:'';
+        $group      =$data[3];
+        $phone      =isset($data[4]) ? $data[4]:'';
+        $chats      =isset($data[5]) ? $data[5]:'';
+        $source     =isset($data[6]) ? $data[6]:'';//渠道
+        $keyword    =isset($data[7]) ? $data[7]:'';
+
+        if(!empty($phone) || !empty($chats)){
+            $model=D('Resource');
+            $area=$model->getArea($address);//获取品牌id
+
+            if($area){
+                $province=$area['id'];//省份id
+                $area_id=$area['area_id'];//区域id
+            }else{
+                $province=0;
+                $area_id=0;
+            }
+
+            $brand_id=$this->getBrandId($keyword);//品牌id
+            $group_id=$model->allocationGroup($brand_id,$area_id);//部门id
+
+            if($group_id > 0){
+                $status=2;
+            }else{
+                $status=1;
+            }
+
+            $map['number_id']       =$number_id;
+            $map['custormer_info']  =$kefu_id;
+            $map['username']        =$username;
+            $map['phone']           =$phone;
+            $map['chats']           =$chats;
+            $map['brand_id']        =$brand_id;
+            $map['area_id']         =$area_id;
+            $map['group_id']        =$group_id;
+            $map['province']        =$province;
+            $map['source']          =$source;
+            $map['keywork']         =$keyword;
+            $map['status']          =$status;
+            $map['time']            =time();
+
+            M('Import')->add($map);
+
+        }
+
+
+
+        return true;
+    }
+
+
+    /*
+     * 获取品牌id
+     * @param $keywork 关键字
+     * @param $talk_page 咨询地址
+     * */
+    public function getBrandId($keywork)
+    {
+        $brand=M('Brands')->select();
+        foreach($brand as $key=>$val){
+            $pos=strpos($keywork,$val['name']);
+            if($pos !== false){
+                return $val['id'];
+            }
+        }
+
+        return 0;
+    }
+
+    /*
+     * 判断表格 栏目标题是否合法
+     * $array 一维数组
+     * */
+    public function existTitle($array)
+    {
+        if($array[0] !='客服ID'){
+            throw new Exception('文件格式不正确');
+        }
+
+        if($array[1] !='客户姓名'){
+            throw new Exception('文件格式不正确');
+        }
+
+        if($array[2] !='地区'){
+            throw new Exception('文件格式不正确');
+        }
+
+        if($array[3] !='部门'){
+            throw new Exception('文件格式不正确');
+        }
+
+        if($array[4] !='电话号码'){
+            throw new Exception('文件格式不正确');
+        }
+
+        if($array[5] !='QQ/微信'){
+            throw new Exception('文件格式不正确');
+        }
+
+        if($array[6] !='来源渠道'){
+            throw new Exception('文件格式不正确');
+        }
+
+        if($array[7] !='关键字'){
+            throw new Exception('文件格式不正确');
+        }
+
+    }
+
 }
